ced008970822df3e1c3d647394dde9a2e0b470a7
SeokhaKang <SeokhaKangCS@gmail.com>
2021-10-14 10:11:11 -0700

    Add user input

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 
 int main()
 {
-	int main(int argv, char** argc) {
-		std::cout << "Count of output: " << count(echo(argv, argc)) << std::endl; 
+	std::string name;
+	std::cout << "What is your name?" <<std::endl;
+	std::cin >> name;
+	std::cout << "Hello " << name << "! << "std::endl;
+	
+	return 1;
 }
+
+
